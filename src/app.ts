fb4d1dea9ec7ea34c2a7e5368de96294d3f86280
Rim Earthlights <iris.wind.code@gmail.com>
2022-12-27 07:16:34 +0900

    Edit OpenAPI

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import Express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import 'dayjs/locale/ja';
+import { routers } from './routers';
+import { CONFIG } from './config/config';
+
+// express/helmet/cors
+const app = Express();
+app.use(helmet());
+app.use(cors());
+
+// ejs
+app.set('view engine', 'ejs');
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// port
+const port = CONFIG.PORT;
+// const user = process.env.USER ? process.env.USER : 'default';
+const hostName = CONFIG.HOSTNAME;
+
+// route settings in routers.ts
+app.use('/', routers);
+
+// No match uri
+app.use((req, res) => {
+    res.status(404).send({ status: 404, message: 'NOT FOUND' });
+});
+
+// launch server.
+app.listen(port, hostName);
