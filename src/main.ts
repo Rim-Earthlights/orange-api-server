9b7d17cf71dc5e23cfadd2298667b151309bdd59
Rim Earthlights <iris.wind.code@gmail.com>
2024-07-23 14:03:05 +0900

    set formatter

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,24 @@ async function bootstrap() {
   const app = await NestFactory.create(RouterModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Orange API')
-  .setDescription('API of Discord Bot for Orange-Server.')
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Orange API')
+    .setDescription('API of Discord Bot for Orange-Server.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  writeFileSync(path.resolve(process.cwd(), 'docs/openapi.yaml'), dump(document, {}), { encoding: 'utf8'});
-  writeFileSync(path.resolve(process.cwd(), 'docs/openapi.json'), JSON.stringify(document, null, 2), { encoding: 'utf8'});
+
+  writeFileSync(
+    path.resolve(process.cwd(), 'docs/openapi.yaml'),
+    dump(document, {}),
+    { encoding: 'utf8' },
+  );
+  writeFileSync(
+    path.resolve(process.cwd(), 'docs/openapi.json'),
+    JSON.stringify(document, null, 2),
+    { encoding: 'utf8' },
+  );
 
   SwaggerModule.setup('docs', app, document);
 
