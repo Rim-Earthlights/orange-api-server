94c8f7cfe8c1bbbce10757849ffab30f7929b6cd
Rim Earthlights <iris.wind.code@gmail.com>
2024-07-23 14:16:34 +0900

    swagger定義追加

--- src/modules/chat/controllers/chat.controller.ts
+++ src/modules/chat/controllers/chat.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../../../guards/auth.guard';
 import { ChatService } from '../services/chat.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('chat')
 @Controller('/chat')
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 export class ChatController {
-  constructor(private readonly chatService: ChatService) {}
+  constructor(private readonly chatService: ChatService) { }
 
   @Get('/test')
+  @ApiOperation({ summary: 'Test' })
+  @ApiResponse({ status: 200, description: 'OK' })
   test(): string {
     return this.chatService.test();
   }
